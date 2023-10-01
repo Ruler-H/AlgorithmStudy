70656f3250a9e9b6cbcd7e220566dc9b684ea2d0
hwangbyeongheon <elwl5515@gmail.com>
2023-10-01 22:04:25 +0900

    ADD: Algorithm Study file

--- algo_cheatsheet.py
+++ algo_cheatsheet.py
@@ -8,3 +8,30 @@ def factorial(n):
         factorial_num *= i
     
     return factorial_num
+
+def unique_check(relation):
+    '''
+        아규먼트의 요소들의 유일성을 체크해서 bool 자료형으로 리턴하는 함수
+    '''
+    rel_set = set(relation)
+
+    return len(relation) == len(rel_set)
+
+def minimal_check(candi_list, relation):
+    '''
+        candi_list 아규먼트 리스트 요소인 리스트 중에
+        요소가 모두 relation에 포함된 요소만 가지고 있는
+        리스트가 있는지 확인한 후
+        있다면 => False
+        없다면 => True
+        를 Return
+    '''
+    for i in candi_list:
+        is_contain = True
+        for j in i:
+            if j not in relation:
+                is_contain = False
+                break
+        if is_contain:
+            return False
+    return True
