f75bcbc2cd5238246791e8e0f385d81a15cf8048
byeongheon.hwang <elwl5515@gmail.com>
2023-11-18 10:27:59 +0900

    ADD: Algorithm Study file

--- algo_cheatsheet/algo_cheatsheet.py
+++ algo_cheatsheet/algo_cheatsheet.py
@@ -10,7 +10,7 @@ def factorial(num):
 
 def unique_check(relation):
     '''
-        아규먼트의 요소들의 유일성을 체크해서 bool 자료형으로 리턴하는 함수
+    아규먼트의 요소들의 유일성을 체크해서 bool 자료형으로 리턴하는 함수
     '''
     rel_set = set(relation)
 
@@ -18,12 +18,12 @@ def unique_check(relation):
 
 def minimal_check(candi_list, relation):
     '''
-        candi_list 아규먼트 리스트 요소인 리스트 중에
-        요소가 모두 relation에 포함된 요소만 가지고 있는
-        리스트가 있는지 확인한 후
-        있다면 => False
-        없다면 => True
-        를 Return
+    candi_list 아규먼트 리스트 요소인 리스트 중에
+    요소가 모두 relation에 포함된 요소만 가지고 있는
+    리스트가 있는지 확인한 후
+    있다면 => False
+    없다면 => True
+    를 Return
     '''
     for i in candi_list:
         is_contain = True
@@ -34,6 +34,23 @@ def minimal_check(candi_list, relation):
         if is_contain:
             return False
     return True
+
+def prime_mul_measure(prime_mul):
+    '''
+    소수 곱으로 이루어진 수에서
+    약수 중 작은 수를 Return
+    '''
+    max_iter = int(prime_mul ** 0.5)
+    prime_array = [True for i in range(max_iter + 1)]
+
+    for i in range(2, max_iter + 1):
+        if prime_array[i] == True:
+            j = 2
+            if prime_mul % i == 0:
+                return i
+            while i * j <= max_iter:
+                prime_array[i * j] = False
+                j += 1
 
 class TrieNode:
     '''Trie 자료구조에서 사용되는 Node'''
